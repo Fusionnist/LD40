234a655408758bed1503715db29b247a33ad9c61
Louis Vassaux <louis.vassaux@sfr.fr>
2017-12-02 12:59:49 +0100

    dsq

---
+++ DL40/Tile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace DL40
+{
+    class Tile : Entity
+    {
+        public Tile(TextureDrawer[] texes_, Vector2 pos_): base(texes_, pos_)
+        {
+            isSolid = true;
+        }
+    }
+}
